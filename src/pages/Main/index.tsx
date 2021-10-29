bc458ed778c35d11b7379efdea5e3ca8f536300c
DGUHJH <ghkdwogud852@gmail.com>
2021-10-29 18:03:11 +0900

    Merge branch 'feature/todo' of https://github.com/DGUHJH/humanscape-test into feature/todo

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,8 +1,29 @@
+import Todo from 'components/Todo';
+import { ReducerType } from 'features';
+import { TodoInitialStateType } from 'features/todo/todoSlice';
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import * as Styled from './styled';
 
 const Main = () => {
-  return <Styled.Root></Styled.Root>;
+  const dispatch = useDispatch();
+  const todo = useSelector<ReducerType, TodoInitialStateType>(
+    (state) => state.todo
+  );
+
+  return (
+    <Styled.Root>
+      <Styled.TitleTypo>투두리스트</Styled.TitleTypo>
+      {todo.todoListData?.map((todoData, index: number) => (
+        <Todo
+          id={todoData.id}
+          isDone={todoData.isDone}
+          value={todoData.value}
+          key={`todo_${index}`}
+        />
+      ))}
+    </Styled.Root>
+  );
 };
 
 export default Main;
--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const Root = styled.div``;
+export const Root = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 20px;
+`;
+
+export const TitleTypo = styled.h1``;
