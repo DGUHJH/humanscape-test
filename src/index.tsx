b809309b2cb057a8c6f9a9ea2e2a23590f1dd69e
DGUHJH <ghkdwogud852@gmail.com>
2021-10-29 17:59:21 +0900

    Add : Provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
+import store from 'features';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from 'router/App';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
