81c6d2b4a715790bbf0ac447bba23f8b3f1edff9
DGUHJH <ghkdwogud852@gmail.com>
2021-10-29 16:44:10 +0900

    Add : Title Typo

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import * as Styled from './styled';
 
 const Main = () => {
-  return <Styled.Root></Styled.Root>;
+  return (
+    <Styled.Root>
+      <Styled.TitleTypo>투두리스트</Styled.TitleTypo>
+    </Styled.Root>
+  );
 };
 
 export default Main;
