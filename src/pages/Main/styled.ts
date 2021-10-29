81c018957caf1ba656e6c0e1a189166e1e5a8e4a
DGUHJH <ghkdwogud852@gmail.com>
2021-10-29 17:03:37 +0900

    Add : Title Typo margin-bottom

--- src/pages/Main/styled.ts
+++ src/pages/Main/styled.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Root = styled.div`
   display: flex;
   flex-direction: column;
+  margin-bottom: 20px;
 `;
 
 export const TitleTypo = styled.h1``;
